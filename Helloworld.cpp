886d913d2a9a93baf3be842dd3e51200d9e5e551
chirag4142 <ctekwani051@gmail.com>
2024-08-24 07:48:58 -0700

    Create Helloworld.cpp

---
+++ Helloworld.cpp
@@ -0,0 +1,13 @@
+//Name - Chirag Tekwani
+//PRN - 23070123042
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
